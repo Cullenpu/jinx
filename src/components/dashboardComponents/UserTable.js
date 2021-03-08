8a8c409b9955f083d30e3990864007ec9d97c1a4
Henry Wang <henryhe.wang@mail.utoronto.ca>
2021-03-08 01:02:26 -0500

    Reverted old changes back for UserTable

--- src/components/dashboardComponents/UserTable.js
+++ src/components/dashboardComponents/UserTable.js
@@ -1,74 +1,37 @@
 import React from "react";
 import yitian from "assets/img/users/yitian.png";
-import { Button, Card, CardHeader, Table } from "reactstrap";
+import {Button, Card, CardHeader, Table} from "reactstrap";
 import UserRow from "./UserRow";
 
 const UserTable = () => {
   return (
     <Card>
-      <CardHeader>
-        User Table{" "}
+      <CardHeader>User Table{' '}
         <small className="text-muted text-capitalize">2020-2021</small>
-        <Button style={{ float: "right" }}>Add User</Button>
+        <Button style={{float: 'right'}}>Add User</Button>
       </CardHeader>
       <Table striped>
         <thead>
-          <tr>
-            <th>ID</th>
-            <th>Avatar</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Rating</th>
-            <th>Edit</th>
-          </tr>
+        <tr>
+          <th>ID</th>
+          <th>Avatar</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Rating</th>
+          <th>Edit</th>
+        </tr>
         </thead>
         <tbody>
-          <UserRow
-            id={123}
-            avatar={yitian}
-            name="Yitian Bitian"
-            email="yz@email.com"
-            rating={10}
-          />
-          <UserRow
-            id={234}
-            avatar={yitian}
-            name="Yitian Bitian"
-            email="yz@email.com"
-            rating={10}
-          />
-          <UserRow
-            id={345}
-            avatar={yitian}
-            name="Yitian Bitian"
-            email="yz@email.com"
-            rating={10}
-          />
-          <UserRow
-            id={456}
-            avatar={yitian}
-            name="Yitian Bitian"
-            email="yz@email.com"
-            rating={10}
-          />
-          <UserRow
-            id={567}
-            avatar={yitian}
-            name="Yitian Bitian"
-            email="yz@email.com"
-            rating={10}
-          />
-          <UserRow
-            id={678}
-            avatar={yitian}
-            name="Yitian Bitian"
-            email="yz@email.com"
-            rating={10}
-          />
+        <UserRow id={123} avatar={yitian} name='Yitian Bitian' email="yz@email.com" rating={10}/>
+        <UserRow id={234} avatar={yitian} name='Yitian Bitian' email="yz@email.com" rating={10}/>
+        <UserRow id={345} avatar={yitian} name='Yitian Bitian' email="yz@email.com" rating={10}/>
+        <UserRow id={456} avatar={yitian} name='Yitian Bitian' email="yz@email.com" rating={10}/>
+        <UserRow id={567} avatar={yitian} name='Yitian Bitian' email="yz@email.com" rating={10}/>
+        <UserRow id={678} avatar={yitian} name='Yitian Bitian' email="yz@email.com" rating={10}/>
         </tbody>
       </Table>
     </Card>
-  );
-};
+  )
+}
 
 export default UserTable;

ec3539cb553b809af4193120766876f802c8169d
Cullen Pu <Cullenpu@gmail.com>
2021-03-30 17:57:40 -0400

    Clean up unused code

--- server.js
+++ server.js
@@ -57,50 +57,7 @@ app.use(
   })
 );
 
-function isMongoError(error) {
-  return (
-    typeof error === "object" &&
-    error !== null &&
-    error.name === "MongoNetworkError"
-  );
-}
-
-const mongoChecker = (req, res, next) => {
-  // check mongoose connection established.
-  if (mongoose.connection.readyState != 1) {
-    console.log("Issue with mongoose connection");
-    res.status(500).send("Internal server error");
-    return;
-  } else {
-    next();
-  }
-};
-
-// Middleware for authentication of resources
-const authenticate = (req, res, next) => {
-  if (env !== "production" && USE_TEST_USER) req.session.user = TEST_USER_ID; // test user on development. (remember to run `TEST_USER_ON=true node server.js` if you want to use this user.)
-
-  if (req.session.user) {
-    User.findById(req.session.user)
-      .then((user) => {
-        if (!user) {
-          return Promise.reject();
-        } else {
-          req.user = user;
-          next();
-        }
-      })
-      .catch((error) => {
-        res.status(401).send("Unauthorized");
-      });
-  } else {
-    res.status(401).send("Unauthorized");
-  }
-};
-
 /** Routes *******************************************************************/
-
-// Add routes here
 
 app.use("/companies", companies);
 app.use("/posting", posting);

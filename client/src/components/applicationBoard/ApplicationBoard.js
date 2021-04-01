c2506c0279c8a5d40984f89954442f127493d518
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2021-04-01 12:02:47 -0400

    can add an application from the front end

--- client/src/components/applicationBoard/ApplicationBoard.js
+++ client/src/components/applicationBoard/ApplicationBoard.js
@@ -10,8 +10,45 @@ import InterviewCol from "./InterviewCol";
 import OfferCol from "./OfferCol";
 import RejectedCol from "./RejectedCol";
 import CompanyModalBody from "./CompanyModalBody";
+import axios from 'axios';
 
 class ApplicationsBoard extends React.Component {
+  constructor(props) {
+    super(props);
+    // for form
+    this.state = {
+      inputCompany: '',
+      inputRole: '',
+      inputStatus: '',
+    };
+
+    this.handleCompanyChange = this.handleCompanyChange.bind(this);
+    this.handleRoleChange = this.handleRoleChange.bind(this);
+    this.handleStatusChange = this.handleStatusChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleCompanyChange(event) {
+    this.setState({inputCompany: event.target.value});
+  }
+  handleRoleChange(event) {
+    this.setState({inputRole: event.target.value});
+  }
+  handleStatusChange(event) {
+    this.setState({inputStatus: event.target.value});
+  }
+
+  handleSubmit(event) {
+    axios.post('http://localhost:5000/applications', {
+      userId: '6064afaf0479d00f99790b69',
+      company: this.state.inputCompany,
+      role: this.state.inputRole,
+      status: this.state.inputStatus
+    })
+    .then(function (response) {
+      window.location.reload()
+    })
+  }
 
   state = {
     modal: false,
@@ -52,10 +89,20 @@ class ApplicationsBoard extends React.Component {
           className={this.props.className}>
           <ModalHeader toggle={this.toggle()}>Add Job Application</ModalHeader>
           <ModalBody>
-            <CompanyModalBody />
+            <CompanyModalBody 
+              inputCompany={this.state.inputCompany} 
+              inputRole={this.state.inputRole} 
+              inputStatus={this.state.inputStatus}
+              handleCompanyChange={this.handleCompanyChange}
+              handleRoleChange={this.handleRoleChange}
+              handleStatusChange={this.handleStatusChange}
+              />
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.toggle()}>
+            <Button color="primary" onClick={() => {
+              this.toggle();
+              this.handleSubmit();
+            }}>
               Save Job
             </Button>{' '}
             <Button color="secondary" onClick={this.toggle()}>

ec5f4e1bdfda0d2e083695d061f8420874dcd4ec
Henry Wang <henryhe.wang@mail.utoronto.ca>
2021-04-04 03:50:54 -0400

    added patch route

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,16 @@
 
 const express = require("express");
 
-const { User } = require("../models/User");
+const {User} = require("../models/User");
 
-const { isMongoError, mongoChecker, authenticate } = require("./utils");
+const {isMongoError, mongoChecker, authenticate} = require("./utils");
 
 const router = express.Router();
+
+// to validate object IDs
+const {ObjectID} = require('mongodb')
+// const {mongoose} = require('./db/mongoose')
+const log = console.log
 
 router.use(mongoChecker);
 
@@ -21,7 +26,7 @@ router.post("/login", (req, res) => {
       // We can check later if this exists to ensure we are logged in.
       req.session.user = user._id;
       req.session.email = user.email;
-      res.send({ id: user._id, email: user.email, name: user.name });
+      res.send({id: user._id, email: user.email, name: user.name});
     })
     .catch((error) => {
       console.log(error);
@@ -68,14 +73,14 @@ router.post("/", (req, res) => {
 
 // Get all users
 router.get("/", (req, res) => {
-  res.send({ user: req.session.user });
+  res.send({user: req.session.user});
 });
 
 // Get all users
 router.post("/all", authenticate, (req, res) => {
   User.find()
     .then((user) => {
-      res.send({ user });
+      res.send({user});
     })
     .catch((error) => {
       if (isMongoError(error)) {
@@ -85,5 +90,42 @@ router.post("/all", authenticate, (req, res) => {
       }
     });
 });
+
+// Edit specific user
+router.patch("/edit/:id", (req, res) => {
+  const id = req.params.id
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send()
+    return;
+  }
+  // // check mongoose connection established.
+  // if (mongoose.connection.readyState !== 1) {
+  //   log('Issue with mongoose connection')
+  //   res.status(500).send('Internal server error')
+  //   return;
+  // }
+
+  const fieldsToUpdate = {}
+  req.body.map((change) => {
+    const propertyToChange = change.path.substr(1) // getting rid of the '/' character
+    fieldsToUpdate[propertyToChange] = change.value
+  })
+
+  User.findOneAndUpdate({_id: id}, {$set: fieldsToUpdate}, {new: true, useFindAndModify: false}).then((user) => {
+    if (!user) {
+      res.status(404).send('Resource not found')
+    } else {
+      res.send(user)
+    }
+  }).catch((error) => {
+    log(error)
+    if (isMongoError(error)) {
+      res.status(500).send('Internal server error')
+    } else {
+      res.status(400).send('Bad Request')
+    }
+  })
+})
 
 module.exports = router;
